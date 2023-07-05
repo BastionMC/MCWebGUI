b672fa379f90a6006cfe745143aa3daf4f0ce1fa
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2023-07-05 02:54:52 +0200

    Dialog core functionality implemented

--- src/js/mcwebgui/elements/dialog.js
+++ src/js/mcwebgui/elements/dialog.js
@@ -4,14 +4,14 @@ MCWebGUI.Dialog.DialogContainer = document.createElement("div");
 MCWebGUI.Dialog.DialogContainer.id = "dialogContainer";
 document.body.appendChild(MCWebGUI.Dialog.DialogContainer);
 
-MCWebGUI.Dialog.AddNewDialog = function (dialogVariable) {
+MCWebGUI.Dialog.AddNewDialog = function (dialogName, dialogVariable) {
     let newDialog = document.createElement("dialog");
     newDialog.className = "dialogWindow";
     newDialog.innerHTML = `<div class="windowBar"></div><div class="windowPage"></div><div class="windowButtons"></div>`;
     
-    let windowBar = newDialog.querySelector("#windowBar");
-    let windowPage = newDialog.querySelector("#windowPage");
-    let windowButtons = newDialog.querySelector("#windowButtons");
+    let windowBar = newDialog.querySelector(".windowBar");
+    let windowPage = newDialog.querySelector(".windowPage");
+    let windowButtons = newDialog.querySelector(".windowButtons");
 
     windowBar.innerHTML = dialogVariable.title;
 
@@ -36,16 +36,45 @@ MCWebGUI.Dialog.AddNewDialog = function (dialogVariable) {
             windowPage.innerHTML = dialogVariable.content;
             break;
         default:
-            console.error(`Error while trying to add new dialog (${dialogVariable}) to cache: ${dialogVariable.type} is not a valid dialog type. Defaulted to "customHTML".`)
+            console.error(`Error while trying to add new dialog (${dialogName}) to cache: "${dialogVariable.type}" is not a valid dialog type. Defaulted to "customHTML".`)
             windowPage.innerHTML = dialogVariable.content;
             break;
     };
-}
 
-MCWebGUI.ShowDialog = function (dialogVariable) {
-    if (MCWebGUI.Dialog.DialogContainer.querySelector("#" + dialogVariable.toString()) == null) {
-        MCWebGUI.Dialog.AddNewDialog(dialogVariable);
+    let elementButtons = MCWebGUI.ButtonArrayToElements(dialogVariable.buttons);
+    for (let i=0;i<elementButtons.length;i++) {
+        if (elementButtons[i].hasAttribute("close_dialog") && elementButtons[i].getAttribute("close_dialog")) {
+            elementButtons[i].addEventListener("click",function () {
+                newDialog.close();
+            });
+        };
+        windowButtons.appendChild(elementButtons[i]);
+    };
+
+    newDialog.id = dialogName;
+
+    MCWebGUI.Dialog.DialogContainer.appendChild(newDialog);
+    newDialog.show();
+};
+
+MCWebGUI.ShowDialog = function (dialogName, dialogVariable) {
+    const dialogCache = MCWebGUI.Dialog.DialogContainer.childNodes;
+    let useCached = false;
+
+    for (let i=0;i<dialogCache.length;i++) {
+        if (dialogCache[i].id == dialogName) {
+            useCached = true;
+            break;
+        };
+    };
+
+    if (!useCached) {
+        MCWebGUI.Dialog.AddNewDialog(dialogName, dialogVariable);
     } else {
-
+        for (let i=0;i<dialogCache.length;i++) {
+            if (dialogCache[i].id == dialogName) {
+                dialogCache[i].show();
+            };
+        };
     };
 };
