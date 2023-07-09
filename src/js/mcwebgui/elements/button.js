f3953d129361241f647de3292dd9c377353615b2
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2023-07-09 19:44:51 +0200

    Tall buttons, scrollbar removed, script reorganization

--- src/js/mcwebgui/elements/button.js
+++ src/js/mcwebgui/elements/button.js
@@ -17,3 +17,39 @@ class McButton extends HTMLElement {
     }
 };
 customElements.define("mc-button", McButton);
+
+MCWebGUI.ButtonObjectToElement = function (buttonObject) {
+    let button = document.createElement("mc-button");
+    const buttonInfo = buttonObject;
+
+    button.setAttribute("type", buttonInfo.type);
+    button.setAttribute("content", buttonInfo.content);
+    
+    if (buttonInfo.hasOwnProperty("close_dialog")) {
+        if (buttonInfo.close_dialog) {
+            button.setAttribute("close_dialog", buttonInfo.close_dialog);
+        };
+    };
+    if (buttonInfo.hasOwnProperty("tall")) {
+        if (buttonInfo.close_dialog) {
+            button.setAttribute("tall", buttonInfo.close_dialog);
+        };
+    };
+    if (buttonInfo.hasOwnProperty("function")) {
+        button.addEventListener("click", function () {
+            buttonInfo.function();
+        });
+    };
+
+    return button;
+};
+
+MCWebGUI.ButtonArrayToElements = function (buttonArray) {
+    let elementButtons = [];
+    
+    for (let i=0;i<buttonArray.length;i++) {
+        elementButtons.push(MCWebGUI.ButtonObjectToElement(buttonArray[i]));                 
+    };
+
+    return elementButtons;
+};
--- src/js/mcwebgui/main.js
+++ src/js/mcwebgui/main.js
@@ -7,34 +7,3 @@ function doShit () {
 document.getElementById("funnyButton").addEventListener("click", function () {
     doShit();
 });
-
-MCWebGUI.ButtonObjectToElement = function (buttonObject) {
-    let button = document.createElement("mc-button");
-    const buttonInfo = buttonObject;
-
-    button.setAttribute("type", buttonInfo.type);
-    button.setAttribute("content", buttonInfo.content);
-    
-    if (buttonInfo.hasOwnProperty("close_dialog")) {
-        if (buttonInfo.close_dialog) {
-            button.setAttribute("close_dialog", buttonInfo.close_dialog);
-        };
-    };
-    if (buttonInfo.hasOwnProperty("function")) {
-        button.addEventListener("click", function () {
-            buttonInfo.function();
-        });
-    };
-
-    return button;
-};
-
-MCWebGUI.ButtonArrayToElements = function (buttonArray) {
-    let elementButtons = [];
-    
-    for (let i=0;i<buttonArray.length;i++) {
-        elementButtons.push(MCWebGUI.ButtonObjectToElement(buttonArray[i]));                 
-    };
-
-    return elementButtons;
-};
