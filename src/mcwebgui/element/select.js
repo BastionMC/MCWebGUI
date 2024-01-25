f4508e45ba9ef72998b2462a9e3c31dba7cc3c29
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-01-25 21:08:39 +0100

    asdasdasdasdasda

---
+++ src/mcwebgui/element/select.js
@@ -0,0 +1,16 @@
+MCWebGUI.Element.Select = {};
+
+class MCSelect extends HTMLElement {
+    constructor() {
+        super();
+
+        const text = this.innerHTML;
+
+        this.innerHTML = "<select><span class=\"text\"></span></select>";
+        let innerSelect = this.querySelector("select");
+        innerSelect.disabled = this.getAttribute("disabled");
+        innerSelect.querySelector(".text").innerHTML = text;
+    }
+}
+
+customElements.define("mc-select", MCSelect);
