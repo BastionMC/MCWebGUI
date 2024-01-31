22a045f22546332978c420b3f9338c9dbcc2b7b1
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-01-31 22:17:39 +0100

    improved main script, cause it contains useless code

---
+++ src/mcwebgui/desktop/titlebar.js
@@ -0,0 +1,6 @@
+// TODO: If the desktop tag is found in the HTML document,
+// that adds the title bar *above* the body.
+
+// IDK how it will be done
+
+// but it has to be possible, right????
--- src/mcwebgui/main.js
+++ src/mcwebgui/main.js
@@ -1,19 +1,12 @@
 let MCWebGUI = {};
 
 MCWebGUI.Log = function (context, contextColor, message) {
-    console.log("%cMCWebGUI%c" + context, "padding: 4px; border-radius: 4px; background-color: white; color: black; margin-right: 4px;", "padding: 4px; border-radius: 4px; background-color: " + contextColor + "; color: black;", message)
+    console.log("%cMCWebGUI%c" + context, "padding: 4px; border-radius: 4px; background-color: white; color: black; margin-right: 4px;", "padding: 4px; border-radius: 4px; background-color: " + contextColor + "; color: black;", message);
 };
 
-MCWebGUI.Information = {
-    "WindowTitle": document.title
-};
-
-MCWebGUI.Information.WindowTitle = document.title;
-
-if (MCWebGUI.Information.WindowTitle == "") {
-    MCWebGUI.Information.WindowTitle = "MCWebGUI";
-    document.title = MCWebGUI.Information.WindowTitle
-    MCWebGUI.Log("Error Handling", "firebrick", "No \"title\" element found, using default of \"MCWebGUI\"");
+if (document.title == "") {
+    document.title = "MCWebGUI Document";
+    MCWebGUI.Log("Error Handling", "firebrick", "No \"title\" element found, using default of \"" + document.title + "\"");
 };
 
 MCWebGUI.GeneratedCSS = document.createElement("style");
