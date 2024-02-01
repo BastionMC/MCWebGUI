d13be148069c6c0c6488ce695f04b755d39a7868
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-02-01 23:49:34 +0100

    HI WULIAN :3

--- src/mcwebgui/main.js
+++ src/mcwebgui/main.js
@@ -17,3 +17,7 @@ MCWebGUI.ConfiguredCSS.id = "mcwebgui-configured"
 
 document.head.appendChild(MCWebGUI.GeneratedCSS);
 document.head.appendChild(MCWebGUI.ConfiguredCSS);
+
+if (window.location.toString().startsWith("https://mc-web-gui.vercel.app")) {
+    MCWebGUI.Log("Acknowledgement", "pink", "Hello! This is \"mc-web-gui.vercel.app\". We just want to make something clear - this website is NOT hosted by us. It is hosted by @wulian233 on GitHub as a fork of MCWebGUI. We don't have anything against it, but we just want to put this here if someone mistakes this site for legit because of the URL.")
+}
