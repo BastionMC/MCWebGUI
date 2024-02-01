8d1ef557987573ab892d146330387bc0aa34678b
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-02-01 23:28:11 +0100

    dropdown stuff

--- src/mcwebgui/element/dropdown.js
+++ src/mcwebgui/element/dropdown.js
@@ -1,40 +1,12 @@
-/* MCWebGUI.Element.Switch = {}
+MCWebGUI.Element.Dropdown = {}
 
-class MCSwitch extends HTMLElement {
+class MCDropdown extends HTMLElement {
     constructor() {
         super();
 
-        this.innerHTML = "<input type=\"checkbox\"><span class=\"knob\"></span>"
+        this.innerHTML = "<input type=\"select\"><span class=\"knob\"></span>"
         this.querySelector("input").disabled = this.getAttribute("disabled")
-    }
-
-    connectedCallback() {
-        console.log("Custom element added to page.");
-    }
-
-    disconnectedCallback() {
-        console.log("Custom element removed from page.");
-    }
-
-    adoptedCallback() {
-        console.log("Custom element moved to new page.");
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-
-        this.getAttribute("disabled")
-
-        console.log(`Attribute ${name} has changed.`);
     }
 }
 
-customElements.define("mc-switch", MCSwitch); 
-
-
-
-TEMPORARY!!!!!!!!!
-
-
-
-
-*/
+customElements.define("mc-dropdown", MCDropdown);
