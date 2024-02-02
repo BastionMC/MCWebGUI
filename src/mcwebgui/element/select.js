c7873ec80541ef2859d729e17e566f78973cb2ce
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-02-02 23:26:31 +0100

    added the silly little line

--- src/mcwebgui/element/select.js
+++ src/mcwebgui/element/select.js
@@ -1,3 +1,5 @@
+// THIS JAVASCRIPT IS *ALL* TEMPORARY!!! FIX THE CODE LATER JAE
+
 MCWebGUI.Element.Select = {};
 
 class MCSelect extends HTMLElement {
@@ -5,13 +7,16 @@ class MCSelect extends HTMLElement {
         super();
 
         const text = this.innerHTML;
-
         this.innerHTML = "<mc-button>Text</mc-button>".repeat(4);
-
         let innerButtons = this.querySelectorAll("mc-button > button");
 
         for (let i=0;i<innerButtons.length;i++) {
             innerButtons[i].disabled = this.getAttribute("disabled") || false;
+            
+            const line = document.createElement("span");
+            line.className = "line";
+            innerButtons[i].appendChild(line);
+            
             innerButtons[i].addEventListener("click", function () {
                 for (let j=0;j<innerButtons.length;j++) {
                     innerButtons[j].id = "";
