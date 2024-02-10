2f08d3fc073d7eb62d8869c1b4df4373ee717c45
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-02-10 14:36:28 +0100

    a bit of work on selections

--- src/mcwebgui/element/select.js
+++ src/mcwebgui/element/select.js
@@ -16,15 +16,38 @@ class MCSelect extends HTMLElement {
             const line = document.createElement("span");
             line.className = "line";
             innerButtons[i].appendChild(line);
-            
+
             innerButtons[i].addEventListener("click", function () {
                 for (let j=0;j<innerButtons.length;j++) {
                     innerButtons[j].id = "";
-                }
+                };
                 innerButtons[i].id = "selected";
             });
-        }
-    }
-}
+        };
+    };
+};
+
+/* MCSelect.prototype.setTextContent = function (text) {
+    this.querySelector("button > span.text").textContent = text || this.querySelector("button > span.text").textContent;
+};
+MCSelect.prototype.getTextContent = function () {
+    return this.querySelector("button > span.text").textContent || "";
+}; 
+MCSelect.prototype.setInnerHTML = function (html) {
+    this.querySelector("button > span.text").innerHTML = html || this.querySelector("button > span.text").innerHTML;
+};
+MCSelect.prototype.getInnerHTML = function () {
+    return this.querySelector("button > span.text").innerHTML || "";
+}; */
+MCSelect.prototype.setDisabled = function (disabled) {
+    let innerButtons = this.querySelectorAll("mc-button > button");
+
+    for (let i=0;i<innerButtons.length;i++) {
+        innerButtons[i].disabled = disabled || false;
+    };
+};
+MCSelect.prototype.getDisabled = function () {
+    return this.querySelector("button").disabled || false;
+};
 
 customElements.define("mc-select", MCSelect);
